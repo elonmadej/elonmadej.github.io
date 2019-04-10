8bcf0e3f8a5ba6d776824dadbb21e02673e35553
elonmadej <49456094+elonmadej@users.noreply.github.com>
2019-04-10 14:41:16 -0500

    Add files via upload

---
+++ w3-nav-java.js
@@ -0,0 +1,10 @@
+     openPage("Home");
+
+function openPage(pageName) {
+     var i;
+    var x = document.getElementsByClassName("page");
+     for (i = 0; i < x.length; i++) {
+         x[i].style.display = "none"; 
+    }
+     document.getElementById(pageName).style.display = "block"; 
+}
